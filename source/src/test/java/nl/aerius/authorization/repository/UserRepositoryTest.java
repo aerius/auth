b301a3aeee611d069b84c2e735aff466bbb33bfd
Bert Scholten <bscholten@dat.nl>
2023-05-12 16:00:26 +0200

    Test for UserRepository
    Not too sure if this is too much testing. The SQL check for instance might be a bit too specific (though it does give you the opportunity to check if the SQL that would be executed makes sense).

---
+++ source/src/test/java/nl/aerius/authorization/repository/UserRepositoryTest.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright the State of the Netherlands
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+package nl.aerius.authorization.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+import java.util.Set;
+
+import org.jooq.DSLContext;
+import org.jooq.Record1;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.jooq.tools.jdbc.MockConnection;
+import org.jooq.tools.jdbc.MockDataProvider;
+import org.jooq.tools.jdbc.MockResult;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import nl.aerius.authorization.generated.db.Tables;
+import nl.aerius.authorization.generated.db.tables.records.LocalUsersRecord;
+
+@ExtendWith(MockitoExtension.class)
+class UserRepositoryTest {
+
+  @Test
+  void testRetrieveLocalUser() {
+    final MockDataProvider provider = (ctx) -> {
+      final String sql = ctx.sql();
+      assertEquals("""
+          select \
+          "auth"."local_users"."local_user_id", \
+          "auth"."local_users"."username", \
+          "auth"."local_users"."password", \
+          "auth"."local_users"."enabled" \
+          from "auth"."local_users" \
+          where lower("auth"."local_users"."username") = lower(?)""", sql, "Expected SQL");
+      // Mock 1 match
+      final DSLContext create = DSL.using(SQLDialect.POSTGRES);
+      final LocalUsersRecord record = create.newRecord(Tables.LOCAL_USERS)
+          .values(1, "SomeName", "SomePassword", true);
+      return new MockResult[] {new MockResult(record)};
+    };
+    final MockConnection connection = new MockConnection(provider);
+    final DSLContext context = DSL.using(connection, SQLDialect.POSTGRES);
+    final UserRepository userRepository = new UserRepository(context);
+
+    final Optional<LocalUsersRecord> result = userRepository.retrieveLocalUser("SomeName");
+
+    assertTrue(result.isPresent(), "Should be a result");
+  }
+
+  @Test
+  void testRetrieveLocalUserNotFound() {
+    final MockDataProvider provider = (ctx) -> {
+      // Mock 0 rows
+      final DSLContext create = DSL.using(SQLDialect.POSTGRES);
+      return new MockResult[] {new MockResult(0, create.newResult(Tables.LOCAL_USERS))};
+    };
+    final MockConnection connection = new MockConnection(provider);
+    final DSLContext context = DSL.using(connection, SQLDialect.POSTGRES);
+    final UserRepository userRepository = new UserRepository(context);
+
+    final Optional<LocalUsersRecord> result = userRepository.retrieveLocalUser("SomeName");
+
+    assertFalse(result.isPresent(), "Shouldn't be a result");
+  }
+
+  @Test
+  void testRetrieveUserRoles() {
+    final MockDataProvider provider = (ctx) -> {
+      final String sql = ctx.sql();
+      assertEquals("""
+          select "auth"."roles"."code" \
+          from "auth"."roles" \
+          join "auth"."user_roles" using ("role_id") \
+          join "auth"."users" using ("user_id") \
+          join "auth"."identity_providers" \
+          using ("identity_provider_id") \
+          where ("auth"."identity_providers"."name" = ? \
+          and "auth"."users"."identity_provider_reference" = ?)""", sql, "Expected SQL");
+      // Mock 2 matches
+      final DSLContext create = DSL.using(SQLDialect.POSTGRES);
+      final Result<Record1<String>> result = create.newResult(Tables.ROLES.CODE);
+      result.add(create.newRecord(Tables.ROLES.CODE).values("Role1"));
+      result.add(create.newRecord(Tables.ROLES.CODE).values("Role2"));
+      return new MockResult[] {new MockResult(2, result)};
+    };
+    final MockConnection connection = new MockConnection(provider);
+    final DSLContext context = DSL.using(connection, SQLDialect.POSTGRES);
+    final UserRepository userRepository = new UserRepository(context);
+
+    final Set<String> result = userRepository.retrieveUserRoles("SomeId", "SomeName");
+
+    assertEquals(result, Set.of("Role1", "Role2"), "Roles should be retrieved");
+  }
+
+  @Test
+  void testRetrieveUserRolesNonFound() {
+    final MockDataProvider provider = (ctx) -> {
+      // Mock no match
+      final DSLContext create = DSL.using(SQLDialect.POSTGRES);
+      return new MockResult[] {new MockResult(0, create.newResult(Tables.ROLES.CODE))};
+    };
+    final MockConnection connection = new MockConnection(provider);
+    final DSLContext context = DSL.using(connection, SQLDialect.POSTGRES);
+    final UserRepository userRepository = new UserRepository(context);
+
+    final Set<String> result = userRepository.retrieveUserRoles("SomeId", "SomeName");
+
+    assertEquals(result, Set.of(), "Roles should be retrieved");
+  }
+
+  @Test
+  void testRetrieveCompetentAuthorities() {
+    final MockDataProvider provider = (ctx) -> {
+      final String sql = ctx.sql();
+      assertEquals("""
+          select "auth"."competent_authorities"."code" \
+          from "auth"."competent_authorities" \
+          join "auth"."user_competent_authorities" using ("competent_authority_id") \
+          join "auth"."users" using ("user_id") \
+          join "auth"."identity_providers" \
+          using ("identity_provider_id") \
+          where ("auth"."identity_providers"."name" = ? \
+          and "auth"."users"."identity_provider_reference" = ?)""", sql, "Expected SQL");
+      // Mock 2 matches
+      final DSLContext create = DSL.using(SQLDialect.POSTGRES);
+      final Result<Record1<String>> result = create.newResult(Tables.COMPETENT_AUTHORITIES.CODE);
+      result.add(create.newRecord(Tables.COMPETENT_AUTHORITIES.CODE).values("Province"));
+      result.add(create.newRecord(Tables.COMPETENT_AUTHORITIES.CODE).values("Ministery"));
+      return new MockResult[] {new MockResult(2, result)};
+    };
+    final MockConnection connection = new MockConnection(provider);
+    final DSLContext context = DSL.using(connection, SQLDialect.POSTGRES);
+    final UserRepository userRepository = new UserRepository(context);
+
+    final Set<String> result = userRepository.retrieveCompetentAuthorities("SomeId", "SomeName");
+
+    assertEquals(result, Set.of("Province", "Ministery"), "Authorities should be returned");
+  }
+
+  @Test
+  void testRetrieveCompetentAuthoritiesNonFound() {
+    final MockDataProvider provider = (ctx) -> {
+      // Mock no match
+      final DSLContext create = DSL.using(SQLDialect.POSTGRES);
+      return new MockResult[] {new MockResult(0, create.newResult(Tables.COMPETENT_AUTHORITIES.CODE))};
+    };
+    final MockConnection connection = new MockConnection(provider);
+    final DSLContext context = DSL.using(connection, SQLDialect.POSTGRES);
+    final UserRepository userRepository = new UserRepository(context);
+
+    final Set<String> result = userRepository.retrieveCompetentAuthorities("SomeId", "SomeName");
+
+    assertEquals(result, Set.of(), "Authorities should be returned");
+  }
+
+}
