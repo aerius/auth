1279848069d7ec0ed723d268c31feaae6799c5a1
Bert Scholten <bscholten@dat.nl>
2023-05-12 12:22:45 +0200

    Scripts for competent authorities table and initial content

---
+++ source/src/main/resources/db/migration/V20230512_02__initial_competent_authorities.sql
@@ -0,0 +1,19 @@
+/* Add records for competent_authorities */
+
+INSERT INTO competent_authorities (code, description)
+VALUES
+	('PROVINCIE_GRONINGEN', 'Provincie Groningen'),
+	('PROVINCIE_FRIESLAND', 'Provincie Fryslân'),
+	('PROVINCIE_DRENTHE', 'Provincie Drenthe'),
+	('PROVINCIE_OVERIJSSEL', 'Provincie Overijssel'),
+	('PROVINCIE_FLEVOLAND', 'Provincie Flevoland'),
+	('PROVINCIE_GELDERLAND', 'Provincie Gelderland'),
+	('PROVINCIE_UTRECHT', 'Provincie Utrecht'),
+	('PROVINCIE_NOORD_HOLLAND', 'Provincie Noord-Holland'),
+	('PROVINCIE_ZUID_HOLLAND', 'Provincie Zuid-Holland'),
+	('PROVINCIE_ZEELAND', 'Provincie Zeeland'),
+	('PROVINCIE_NOORD_BRABANT', 'Provincie Noord-Brabant'),
+	('PROVINCIE_LIMBURG', 'Provincie Limburg'),
+	('MINISTERIE_I_EN_W', 'Ministerie van Infrastructuur en Waterstaat'),
+	('MINISTERIE_LNV', 'Ministerie van Landbouw, Natuur en Voedselkwaliteit'),
+	('MINISTERIE_DEFENSIE', 'Ministerie van Defensie');
