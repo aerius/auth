7a40941b8753fc42ba0506139166cfe57f4f4733
Bert Scholten <bscholten@dat.nl>
2023-05-24 09:53:29 +0200

    Adjusted testdata script to add more test users
    These new users can be used by our testers to check functionality (as long as we don't have a persistent database it's a bit annoying to lose these every time a deploy is done)

--- source/src/main/resources/db/testdata/afterMigrate.sql
+++ source/src/main/resources/db/testdata/afterMigrate.sql
@@ -2,9 +2,19 @@
 -- Ensure queries are runnable multiple times.
 
 INSERT INTO auth.local_users (username, password)
-	VALUES ('testviewer', '{bcrypt}$2a$12$sG0hLLmx3/3Mjts69Y1Lp.B.EXCH51dnUxkRPXQ2RJ8tsrbU4OsqS')
+VALUES 
+	('testviewer', '{bcrypt}$2a$12$sG0hLLmx3/3Mjts69Y1Lp.B.EXCH51dnUxkRPXQ2RJ8tsrbU4OsqS'),
+	('viewer', '{bcrypt}$2a$12$YcGqW.gqhPYfWpILQGoyv.neAZZsvQBbUPTl0yvDiezOGr779jWnC'),
+	('editor', '{bcrypt}$2a$12$PLYDm40MTcnyza65qQn3xeiWnj.mopUf2eKcjjAHiqBWc31E13jke'),
+	('superuser', '{bcrypt}$2a$12$/Zrv8cXcvruG.eGgCl1yl.QVNsjrmMjOFgj20aKTLvUqypZtJYisC'),
+	('admin', '{bcrypt}$2a$12$.dzrMeJ6s2GzFtB.5uUIsenqYhBxAXLBbRE4pYdKEJAMrPwqGhFEi'),
+	('special', '{bcrypt}$2a$12$1Zqh2OSxja5tYnD3PysgNeunLI7cYcoNfiLUcIZfoxKqg3ZoXccLy')
 	ON CONFLICT DO NOTHING;
 
+WITH test_users AS (
+SELECT 
+	unnest(ARRAY['testviewer', 'viewer', 'editor', 'superuser', 'admin', 'special']) AS username
+)
 INSERT INTO auth.users (identity_provider_id, identity_provider_reference)
 SELECT
 	identity_provider_id,
@@ -12,35 +22,43 @@ SELECT
 
 	FROM auth.identity_providers
 	CROSS JOIN auth.local_users
+		INNER JOIN test_users USING (username)
 
 	WHERE identity_providers.name = 'local'
-		AND local_users.username = 'testviewer'
 	ON CONFLICT DO NOTHING;
 
+WITH test_users AS (
+SELECT 
+	unnest(ARRAY['testviewer', 'viewer', 'editor', 'superuser', 'admin', 'special']) AS identity_provider_reference,
+	unnest(ARRAY['VIEWER', 'VIEWER', 'EDITOR', 'SUPER_USER', 'ADMIN', 'SPECIAL']) AS code
+)
 INSERT INTO auth.user_roles (user_id, role_id)
 SELECT
 	user_id,
 	role_id
 
-	FROM auth.users
+	FROM test_users
+		INNER JOIN auth.users USING (identity_provider_reference)
 		INNER JOIN auth.identity_providers USING (identity_provider_id)
-	CROSS JOIN auth.roles
+		INNER JOIN auth.roles USING (code)
 
 	WHERE identity_providers.name = 'local'
-		AND users.identity_provider_reference = 'testviewer'
-		AND roles.code = 'VIEWER'
 	ON CONFLICT DO NOTHING;
 
+WITH test_users AS (
+SELECT 
+	unnest(ARRAY['testviewer', 'viewer', 'editor', 'superuser', 'admin', 'special']) AS identity_provider_reference,
+	unnest(ARRAY['PROVINCIE_OVERIJSSEL', 'PROVINCIE_UTRECHT', 'PROVINCIE_UTRECHT', 'MINISTERIE_LNV', 'MINISTERIE_LNV', 'MINISTERIE_LNV']) AS code
+)
 INSERT INTO auth.user_competent_authorities (user_id, competent_authority_id)
 SELECT
 	user_id,
 	competent_authority_id
 
-	FROM auth.users
+	FROM test_users
+		INNER JOIN auth.users USING (identity_provider_reference)
 		INNER JOIN auth.identity_providers USING (identity_provider_id)
-	CROSS JOIN auth.competent_authorities
+		INNER JOIN auth.competent_authorities USING (code)
 	
 	WHERE identity_providers.name = 'local'
-		AND users.identity_provider_reference = 'testviewer'
-		AND competent_authorities.code = 'PROVINCIE_OVERIJSSEL'
 	ON CONFLICT DO NOTHING;
