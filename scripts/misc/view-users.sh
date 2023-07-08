a73ace86924a0a12e24c1d4213fa3bbddc3b0b3f
JornC <jorncruijsen@gmail.com>
2023-07-08 13:08:57 +0200

    Add some convenience scripts

---
+++ scripts/misc/view-users.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Database details
+DBNAME="aerius_authorization"
+DBUSER="aerius"
+DBPASS="aerius"
+DBHOST="localhost"  # Assuming your database is on the same machine. Change if needed.
+
+export PGPASSWORD=$DBPASS
+
+# SQL Command to list roles from the auth.roles table
+SQL_COMMAND="SELECT * FROM auth.users;"
+
+# Execute SQL Command
+psql -h $DBHOST -U $DBUSER -d $DBNAME -c "$SQL_COMMAND"
+
+# Unset the PGPASSWORD environment variable
+unset PGPASSWORD
